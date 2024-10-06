0cfcdb004764b4e3813617d41ac602c40906e796
Karlo Perkovic <perkovickarlo5@gmail.com>
2024-10-06 21:58:24 +0200

    api za update frameBuilder-a

--- server/server.js
+++ server/server.js
@@ -47,6 +47,18 @@ app.post("/frameBuilders",async (req, res) =>{
   }
 })
 
+app.put('/frameBuilders/:pero', async (req, res) => {
+  try {
+    const frameBuilderId= req.params.pero
+    console.log("frameBuilder id",frameBuilderId)
+    const result = await FrameBuilder.findByIdAndUpdate(frameBuilderId, req.body, { new: true })
+    res.status(200).json(result)
+  } catch (err) {
+      res.status(500).json({ message: 'Error updating frameBuilder', error: err.message });
+  }
+});
+
+
 // start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);

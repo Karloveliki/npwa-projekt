03f9c1d02acbab261b32a25c2c53163eda5885f6
Karlo Perkovic <perkovickarlo5@gmail.com>
2024-11-16 10:43:24 +0100

    dodan loading,handlanje i ispis errora pri fetchu

--- client/src/components/Frame.js
+++ client/src/components/Frame.js
@@ -2,58 +2,47 @@ import { useState } from "react";
 import ForOneFrame from "./ForOneFrame"
 function Frame(){
     const [allFrames,setAllFrames]=useState([])
-
+    const [greska,setGreska]=useState("")
+    const [loading,setLoading]=useState(false)
     async function catchFrame(){
-        console.log("dogvaća")
+       
+        try{
+            console.log("dogvaća")
 
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        const response = await fetch("http://localhost:5050/frames", requestOptions)
-        console.log("response: ",response)
-        if(response.ok){
-            const json=await response.json()
-            setAllFrames(json)
-            console.log("u jsonu: ",json)
+            const requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+            };
+            setLoading(true)
+            const response = await fetch("http://localhost:5050/frames", requestOptions)
+            
+            console.log("response: ",response)
+            if(response.ok){
+                const json=await response.json()
+                setAllFrames(json)
+                setLoading(false)
+                console.log("u jsonu: ",json)
+            }
+            else{
+                throw new Error("response nije dohvacen")
+            }
         }
-        
+        catch(err){
+            //console.log("greska",err)
+            setGreska(err.message)
+            setLoading(false)
+        }
     }
 
 
 
 
-    const frames=[{
-        name: "bika1",
-        bikeType: "road",
-        geometry_type: "man",
-        wheelSize: '20"',
-        suspension: "no",
-        material: "steel",
-        availableSizes: ["47","50","52"],
-        
-    },
-    {
-        name:"bika2",
-        bikeType: "mtb",
-        geometry_type: "women",
-        suspension: "front",
-        material: "titan",
-        availableSizes: ["54","60","62"]
-    },
-    {
-        name:"bika3",
-        bikeType: "treking",
-        geometry_type: "universal",
-        suspension: "back",
-        material: "aluminium",
-        availableSizes: ["62","47","54"]
-    }
-    ]
+    
     return <div>
         {allFrames.map((frame=>{return <ForOneFrame key={frame._id} frameDic={frame}></ForOneFrame>}))}
         <button onClick={()=>{catchFrame()}}>dohFrame</button>
+        {loading ? <div>loading</div>:null}
+        {greska ? <p>{greska}</p> : null}
     </div>
 }
 

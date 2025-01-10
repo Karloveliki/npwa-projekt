2af4e04c25b67548c42570ff17bb703fc23d35c7
Karlo Perkovic <perkovickarlo5@gmail.com>
2025-01-10 12:38:20 +0100

    izmjena na kosarici

--- client/src/App.js
+++ client/src/App.js
@@ -14,61 +14,80 @@ import KosaricaPage from './components/KosaricaPage';
 
 function App() {
   const [frameBuilder,setFrameBuilder]=useState({})
-  const [kosarica, setKosarica] = useState([])
+  const [kosarica, setKosarica] = useState({sadrzaj:[]})
 
-  const pocetnaKosarica = {
-    sadrzaj:kosarica,
-    dodajFrame: (frame) => {
-      for(let i=0;i<kosarica.length;i++){
-        if(kosarica[i].frame._id==frame._id){
-            const noviItem={frame,kolicina: kosarica[i].kolicina+1}
-            const novaKosarica=[...kosarica]
-            novaKosarica[i]=noviItem
-            setKosarica(novaKosarica)
-            return
+  const dodajFrame = (frame) => {
+    console.log("dodaj frame")
+    for(let i=0;i<kosarica.sadrzaj.length;i++){
+      if(kosarica.sadrzaj[i].frame._id==frame._id){
+          const noviItem={frame,kolicina: kosarica.sadrzaj[i].kolicina+1}
+          const noviSadrzaj=[...kosarica.sadrzaj]
+          noviSadrzaj[i]=noviItem
+          setKosarica({...kosarica, sadrzaj:noviSadrzaj})
+          console.log("u dodaj frame2", noviSadrzaj, kosarica)
+          return
         }
-      } 
-      setKosarica([...kosarica, { frame, kolicina:1}]) 
-    },
-    brisanjeSve: ()=>{setKosarica([])},
-    brisanjeStavke: (ind)=>{ let cop=[...kosarica]
-                              cop.splice(ind,1)
-                              setKosarica(cop)
-    },
-    izracunajPrice: ()=>{
-      let price=0
-      for(let i=0;i<kosarica.length;i++){
-        const item=kosarica[i]
-        price+=item.frame.basePrice*item.kolicina
-      }
-      return price
-    },
-    izracunajKolicinu: ()=>{
-      let kolicina=0
-      for(let i=0;i<kosarica.length;i++){
-        kolicina+=kosarica[i].kolicina
-      }
-      return kolicina
-    },
-    izracunKolicineTogFramea: (frameId)=>{
-      let kolicina=0
-      for(let i=0;i<kosarica.length;i++){
-        const item=kosarica[i]
-        if(item.frame._id==frameId){
-          kolicina+=item.kolicina
-        }
-      }
-      return kolicina
-    }
+    } 
+    console.log("Za prvi", kosarica)
+    const noviSadrzaj = [...kosarica.sadrzaj]
+    noviSadrzaj.push({ frame, kolicina:1})
+    setKosarica({...kosarica, sadrzaj: noviSadrzaj}) 
+    console.log("u dodaj frame1", kosarica)
   }
-  
+
+  const brisanjeSve = () =>{setKosarica({...kosarica, sadrzaj: []} )}
+
+  const brisanjeStavke = (ind)=>{ let noviSadrzaj=[...kosarica.sadrzaj]
+    noviSadrzaj.splice(ind,1)
+    setKosarica({...kosarica, sadrzaj:noviSadrzaj})
+  }
+
+  const izracunajPrice = ()=>{
+    let price=0
+    for(let i=0;i<kosarica.sadrzaj.length;i++){
+      const item=kosarica.sadrzaj[i]
+      price+=item.frame.basePrice*item.kolicina
+    }
+    return price
+  }
+
+  const izracunajKolicinu = ()=>{
+    let kolicina=0
+    for(let i=0;i<kosarica.sadrzaj.length;i++){
+      kolicina+=kosarica.sadrzaj[i].kolicina
+    }
+    return kolicina
+  }
+
+  const izracunKolicineTogFramea = (frameId)=>{
+    let kolicina=0
+    for(let i=0;i<kosarica.sadrzaj.length;i++){
+      const item=kosarica.sadrzaj[i]
+      if(item.frame._id==frameId){
+        kolicina+=item.kolicina
+      }
+    }
+    return kolicina
+  }
+
+  const kosaricaValue = {
+    ...kosarica,
+    dodajFrame,
+    brisanjeSve,
+    brisanjeStavke,
+    izracunajPrice,
+    izracunajKolicinu,
+    izracunKolicineTogFramea
+  }  
+
+  console.log("inicijalna kosarica, ", kosaricaValue)
   function ispisFrameBuildera(s){
     setFrameBuilder(s)
     console.log("mjenja se na: ",s)
   }
   return (
     <div className="App">
-      <KosaricaContext.Provider value={pocetnaKosarica}>
+      <KosaricaContext.Provider value={kosaricaValue}>
         <header className="App-header"> 
           <div>
             <Router>

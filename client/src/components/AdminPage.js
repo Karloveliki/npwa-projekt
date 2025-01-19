972c38a41cd6808cba7a4c8c4cbf391d9e01b87c
Karlo Perkovic <perkovickarlo5@gmail.com>
2025-01-19 21:22:32 +0100

    dohvaćaju se framebuilderi u adminu

--- client/src/components/AdminPage.js
+++ client/src/components/AdminPage.js
@@ -1,6 +1,52 @@
-
+import { useState,useEffect } from "react";
 
 function AdminPage(){
-    return <div>U adminu sam</div>
+    const [frameBuilders,setFrameBuilders]=useState(null)
+    const [load,setLoad]=useState(false)
+    const [greska,setGreska]=useState(false)
+
+    async function getFrameBuilders(){
+        setGreska(false)
+        console.log("getframebuilderes")
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        setLoad(true)
+        const response = await fetch("http://localhost:5050/frameBuilders", requestOptions)
+        setLoad(false)
+        if(response.ok){
+            const responseData=await response.json()
+            setFrameBuilders(responseData)
+            console.log("response: ",responseData)
+        }
+        else{
+            setGreska(true)
+            setLoad(false)
+        }
+    }
+
+    /*async function deleteFrameBuilder(){
+        const requestOptions = {
+            method: 'DELETE',
+            redirect: 'follow'
+        };
+        const response = await fetch("http://localhost:5050/frameBuilders?", requestOptions)
+    }
+   */
+    useEffect(
+            () => { getFrameBuilders()},
+            []
+        )
+
+
+    return <div>
+            <div>U adminu sam</div>
+            {frameBuilders ? frameBuilders.map((frBuilder)=>{return <div>{frBuilder.name}</div>}) : null}
+            {greska ? <div>greska</div> : null}
+            {load ? <div>loading</div>:null}
+        </div>
+
+    
 }
 export default AdminPage

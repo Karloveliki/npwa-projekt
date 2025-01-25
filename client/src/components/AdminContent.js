0d54c1d305e438c061636a39bd1237c2588a62f4
Karlo Perkovic <perkovickarlo5@gmail.com>
2025-01-25 11:47:20 +0100

    logout se javalja na glavnooj admin stranici nakon logina

--- client/src/components/AdminContent.js
+++ client/src/components/AdminContent.js
@@ -1,17 +1,23 @@
 import { useContext } from "react"
 import KosaricaContext from "../KosaricaContext"
 import { Link } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 
 function AdminContent({children}){
     const context=useContext(KosaricaContext)
     const user=context.user
+    const navigate = useNavigate();
+    function adminLogout(){
+        context.setUser(null)
+        navigate('/')
+    }
     if(!user || user.userType!="admin"){
         return <div><div>Neispravan login admina</div>
                 <Link to={'/login'}>ulogirajte se kao admin</Link>
               </div>
     }
     return <div id="adminPage">
-
+            <button onClick={()=>{adminLogout()}}>Logout</button>
             <div id="adminPageHeading">
                 <Link to={'/admin'}>Admin Home</Link>
             </div>

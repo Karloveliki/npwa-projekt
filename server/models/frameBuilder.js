36fc9f1f00dd6d55a70e3f49a250abef27d8519e
Karlo Perkovic <perkovickarlo5@gmail.com>
2025-01-25 16:22:06 +0100

    javi gresku kada brisem framebuildera u bazi imam njegove bicikle

--- server/models/frameBuilder.js
+++ server/models/frameBuilder.js
@@ -18,11 +18,26 @@ const FrameBuilderSchema = new Schema({
     type: String,
     required: true
   },
-  bikeFrames: {
-    type: [ObjectId],
-    ref: "Frame"
-  }
 });
+                  
 
+FrameBuilderSchema.pre('findOneAndDelete', { document: true, query: true },async function(next){
+  const docToUpdate = await this.model.findOne(this.getQuery());
+  const frameBuilderId = docToUpdate._id;
+  const Frame = mongoose.model("Frame");
+    try{
+      const framesWithThisBuilder = await Frame.countDocuments({frameBuilder: frameBuilderId});
+        if(framesWithThisBuilder>0){
+          console.log("ne smijem ga brisati")
+          next(new Error(`Cannot delete FrameBuilder with ID ${frameBuilderId} as 
+            it is referenced by ${framesWithThisBuilder} frames.`));
+      }
+        next();
+    }
+    catch(e){
+      console.log("neka greska", e)
+      next(e);
+    }
+});
 // Export function to create "SomeModel" model class
 export default  mongoose.model("FrameBuilder", FrameBuilderSchema)

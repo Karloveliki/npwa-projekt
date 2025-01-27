9b2efaf34578b2bae66ed2a1d27d02569182bab1
Karlo Perkovic <perkovickarlo5@gmail.com>
2025-01-27 21:09:40 +0100

    isformatirana registracija

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import KosaricaContext from "../KosaricaContext";
 import { useState , useContext} from "react";
-
+import { useNavigate } from "react-router-dom";
 
 function Registration(){
     const [registered,setRegistered]=useState(false)
     const [greska,setGreska]=useState(false)
+    const navigate=useNavigate()
    async function register(ev){
         ev.preventDefault()
         const form = ev.currentTarget
@@ -24,24 +25,31 @@ function Registration(){
             const response = await fetch(`http://localhost:5050/users`, requestOptions)
         if(!response.ok){
             setGreska(true)
+            setRegistered(false)
         }
-        if(response){
+        if(response.ok){
             setRegistered(true)
         }
     }
     return <div >
-        <h1>Registracija</h1>
-        {registered ? <Link to="/login">Login</Link>:
-        <form onSubmit={(ev)=>{register(ev)}}>
-            <label htmlFor="mail">Email</label><br/>
-            <input type="text" id="mail" name="mail"/><br/>
-            <label htmlFor="uname">Korisnicko ime:</label><br/>
-            <input type="text" id="uname" name="uname"/><br/>
-            <label htmlFor="password">Lozinka</label><br/>
-            <input type="password" id="password" name="password"/><br/>
-            <button type="submit">Registration</button>
-        </form> }
-        {greska ? <div>Greska pri registraciji</div> : null}
+        {registered ? navigate('/login'):
+            <div className="w3-container w3-display-topmiddle w3-padding-large w3-margin-bottom" >
+                <h1 className="w3-center">Registracija</h1>
+                <div className="w3-card-4 w3-padding-large w3-light-gray">
+                    <form className="w3-container" onSubmit={(ev)=>{register(ev)}}>
+                        <label htmlFor="mail">Email</label><br/>
+                        <input className="w3-input w3-border w3-margin-bottom" type="text" id="mail" name="mail"/><br/>
+                        <label htmlFor="uname">Korisnicko ime:</label><br/>
+                        <input className="w3-input w3-border w3-margin-bottom" type="text" id="uname" name="uname"/><br/>
+                        <label htmlFor="password">Lozinka</label><br/>
+                        <input className="w3-input w3-border w3-margin-bottom" type="password" id="password" name="password"/><br/>
+                        <button type="submit">Registration</button>
+                    </form>
+                </div>
+                {greska ? <div className="w3-large w3-red">Greska pri registraciji</div> : null}
+            </div>
+        }
+       
 
     </div>
 }

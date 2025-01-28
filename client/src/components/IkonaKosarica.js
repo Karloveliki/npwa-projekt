b356eecd21f0f30b459cb5d6d888cba49aa87dec
Karlo Perkovic <perkovickarlo5@gmail.com>
2025-01-28 16:01:09 +0100

    kosarica ikona koji je link na kosaricu

--- client/src/components/IkonaKosarica.js
+++ client/src/components/IkonaKosarica.js
@@ -1,6 +1,9 @@
 import { useState , useContext} from "react";
 import KosaricaContext from "../KosaricaContext";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { CiShoppingBasket } from "react-icons/ci";
 
 function IkonaKosarica(){
     const kosarica=useContext(KosaricaContext)
@@ -10,13 +13,11 @@ function IkonaKosarica(){
             <div>
             <div>kolicina:{kosarica.izracunajKolicinu()}</div> 
             <p>price:{kosarica.izracunajPrice()}</p>
-            <Link to={"/shoppingCart"}>KOsarica detalji</Link>
+            <Link to={"/shoppingCart"}><CiShoppingBasket style={{ fontSize: '3em' }} /></Link>
             </div>:
             <div>Kosarica je prazna</div>
         } 
             
-            
-    
     </div>
 }
 export default IkonaKosarica

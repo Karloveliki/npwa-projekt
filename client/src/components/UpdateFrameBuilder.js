6f95df3ff95c304387809883dd30d4ed2a60a7ad
Karlo Perkovic <perkovickarlo5@gmail.com>
2025-01-28 21:09:49 +0100

    frameBuilder podaci se pojavljuju u editu

--- client/src/components/UpdateFrameBuilder.js
+++ client/src/components/UpdateFrameBuilder.js
@@ -1,4 +1,40 @@
+import { useParams } from "react-router-dom"
+import { useContext, useEffect, useState } from "react"
+import KosaricaContext from '../KosaricaContext';
 function UpdateFrameBuilder(){
-    return <dvi>ja sam edit frameBuildera</dvi>
+    const params = useParams()
+    const frameBuilderId=params.frameBuilderId
+    const context=useContext(KosaricaContext)
+    const user=context.user
+    const [frameBuilder,setFrameBuilder]=useState(null)
+
+
+    async  function getFrameBuilder(){
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: {
+                'Content-Type': 'application/json', // Tell the server the data format
+                'Authorization': `Bearer ${user.token}`
+              }
+        };
+    
+        const response = await fetch(`http://localhost:5050/frameBuilders/${frameBuilderId}`, requestOptions)
+        const dataResponse= await response.json()
+        console.log("frameBuilder:   ",dataResponse)
+        setFrameBuilder(dataResponse)
+    }
+
+    useEffect(
+        () => {getFrameBuilder()},
+        []
+     )
+     if(!frameBuilder){
+        return <div>Loading</div>
+     }
+    return <div>
+        <dvi>ja sam edit frameBuildera {frameBuilderId}</dvi>
+            <div>{frameBuilder.name}</div>
+        </div>
 }
 export default UpdateFrameBuilder
